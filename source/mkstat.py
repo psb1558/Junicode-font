55091b3e3962f222c7cbcbfb84e48f4c295843b2
Peter Baker <b.tarde@gmail.com>
2023-08-04 03:58:17 -0400

    Corrected names in STAT; use format 1 instead of 2 axis value tables.

--- source/mkstat.py
+++ source/mkstat.py
@@ -22,12 +22,12 @@ weightDict = dict(
     tag="wght",
     name="Weight",
     values=[
-        dict(nominalValue=300, name="Light", rangeMinValue=300, rangeMaxValue=350),
-        dict(nominalValue=400, name="Regular", flags=0x2, rangeMinValue=350, rangeMaxValue=450),
-        dict(nominalValue=500, name="Medium", rangeMinValue=450, rangeMaxValue=550),
-        dict(nominalValue=600, name="SemiBold", rangeMinValue=550, rangeMaxValue=650),
-        dict(nominalValue=700, name="Bold", rangeMinValue=650, rangeMaxValue=750),
-        dict(value=400, name="Regular", linkedValue=700, flags=0x2)
+        dict(value=300, name="Light"),
+        dict(value=400, name="Regular", linkedValue=700, flags=0x2),
+        dict(value=500, name="Medium"),
+        dict(value=600, name="SmBold"),
+        dict(value=700, name="Bold"),
+        # dict(value=400, name="Regular", linkedValue=700, flags=0x2)
     ]
 )
 
@@ -35,17 +35,11 @@ widthDict = dict(
     tag="wdth",
     name="Width",
     values=[
-#       dict(nominalValue=60, name="Condensed", rangeMinValue=60, rangeMaxValue=70),
-#       dict(nominalValue=80, name="SemiCondensed", rangeMinValue=70, rangeMaxValue=90),
-#       dict(nominalValue=100, name="Normal", flags=0x2, rangeMinValue=90, rangeMaxValue=110),
-#       dict(nominalValue=120, name="SemiExpanded", rangeMinValue=110, rangeMaxValue=125),
-#       dict(nominalValue=140, name="Expanded", rangeMinValue=125, rangeMaxValue=140),
-# Recalculated, since glyphslib changes scale to 75-125 with 100 default
-        dict(nominalValue=75, name="Condensed", rangeMinValue=75, rangeMaxValue=81),
-        dict(nominalValue=87.5, name="SemiCondensed", rangeMinValue=81, rangeMaxValue=94),
-        dict(nominalValue=100, name="Normal", flags=0x2, rangeMinValue=94, rangeMaxValue=106),
-        dict(nominalValue=112.5, name="SemiExpanded", rangeMinValue=106, rangeMaxValue=119),
-        dict(nominalValue=125, name="Expanded", rangeMinValue=119, rangeMaxValue=125),
+        dict(value=75, name="Cond"),
+        dict(value=87.5, name="SmCond"),
+        dict(value=100, name="Normal", flags=0x2),
+        dict(value=112.5, name="SmExp"),
+        dict(value=125, name="Exp"),
     ]
 )
 
@@ -53,9 +47,9 @@ enlargeDict = dict(
     tag="ENLA",
     name="Enlarge",
     values=[
-        dict(nominalValue=0, name="Normal", flags=0x2, rangeMinValue=0, rangeMaxValue=33),
-        dict(nominalValue=47, name="Enlarged", rangeMinValue=33, rangeMaxValue=66),
-        dict(nominalValue=100, name="CapSize", rangeMinValue=66, rangeMaxValue=100),
+        dict(value=0, name="Normal", flags=0x2),
+        dict(value=47, name="Enlarged"),
+        dict(value=100, name="CapSize"),
     ]
 )
 
@@ -64,7 +58,7 @@ format2RomanAxes = [
     dict(
         tag="ital",
         name="Italic",
-        values=[dict(value=0, name="Italic", linkedValue=1, flags=0x2)]
+        values=[dict(value=0, name="Roman", linkedValue=1, flags=0x2)]
     )
 ]
 
